74644d58c887adda909dcfa18b5f208cc063dace
Jason Walke <jason.walke@gmail.com>
2019-10-27 10:11:46 -0600

    add color support - format with prettier

--- force-app/main/default/lwc/reveal/reveal.js
+++ force-app/main/default/lwc/reveal/reveal.js
@@ -1,30 +1,35 @@
-import { LightningElement, track , wire} from 'lwc';
-import confetti from '@salesforce/resourceUrl/aa';
-import { loadScript } from 'lightning/platformResourceLoader';
-import getGender from '@salesforce/apex/revealController.getGender';
+import { LightningElement, track, wire } from "lwc";
+import confetti from "@salesforce/resourceUrl/aa";
+import { loadScript } from "lightning/platformResourceLoader";
+import getGender from "@salesforce/apex/revealController.getGender";
 
 export default class Reveal extends LightningElement {
-    @track detailsVisible = false;
-    @wire(getGender) contact;
+  @track detailsVisible = false;
+  @wire(getGender) contact;
 
-    handleClick() {
-        var can;
-        var confettiSettings;
-        var confettiDo; 
-        loadScript(this, confetti + '/bb.js')
-            .then(() => { 
-                can = this.template.querySelector('canvas');
-                confettiSettings = { 
-                    target: can,
-                    max: 200,
-                    props: ['square','line']
-                };
-                
-                confettiDo = new window.ConfettiGenerator(confettiSettings);
-                confettiDo.render();
-                this.detailsVisible = true;
-            });
+  handleClick() {
+    var can;
+    var confettiSettings;
+    var confettiDo;
+    loadScript(this, confetti + "/bb.js").then(() => {
+      can = this.template.querySelector("canvas");
+      confettiSettings = {
+        target: can,
+        max: 200,
+        props: ["square", "line"],
+        colors: this.color
+      };
+
+      confettiDo = new window.ConfettiGenerator(confettiSettings);
+      confettiDo.render();
+      this.detailsVisible = true;
+    });
+  }
+
+  get color() {
+    if (this.contact.data.Gender__c === "boy") {
+      return [[40, 85, 209], [15, 78, 252], [135, 167, 255]];
     }
-
-    
+    return [[245, 87, 66], [255, 84, 84], [255, 166, 166]];
+  }
 }
